423c9964dab89a712213941e968a8bd848e69324
Henok <49408901+henokcreative@users.noreply.github.com>
2026-02-20 23:58:58 +0200

    allowed host

--- labio/settings.py
+++ labio/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-^ut_2)(-tv66#07eam8dr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['labio-backend.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
