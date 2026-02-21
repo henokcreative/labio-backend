e5501d888af72b140b74be9f03ebc6627271b5c3
Henok <49408901+henokcreative@users.noreply.github.com>
2026-02-21 19:50:55 +0200

    add email notification

--- labio/settings.py
+++ labio/settings.py
@@ -156,3 +156,11 @@ STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 CORS_ALLOWED_ORIGINS = [
     'https://labiomedia.vercel.app',
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('henok.karvonen@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('iuiw miff fgso cpup')
+DEFAULT_FROM_EMAIL = os.environ.get('henok.karvonen@gmail.com')
