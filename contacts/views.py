222bf781d2defa0297fc9ac27f7d6416569151c5
Henok <49408901+henokcreative@users.noreply.github.com>
2026-02-21 22:09:21 +0200

    add RESEND

--- contacts/views.py
+++ contacts/views.py
@@ -4,9 +4,12 @@ from django.shortcuts import render
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.core.mail import send_mail
 from .models import ContactMessage
 from .serializers import ContactMessageSerializer
+import resend
+import os
+
+resend.api_key = os.environ.get('RESEND_API_KEY')
 
 @api_view(['POST'])
 def submit_contact(request):
@@ -14,13 +17,16 @@ def submit_contact(request):
     if serializer.is_valid():
         serializer.save()
         try:
-            send_mail(
-                subject=f"New contact from {request.data.get('name')}",
-                message=f"Name: {request.data.get('name')}\nEmail: {request.data.get('email')}\nMessage: {request.data.get('message')}",
-                from_email=None,
-                recipient_list=['henok.karvonen@utu.fi'],
-            )
+            resend.Emails.send({
+                "from": "onboarding@resend.dev",
+                "to": "henok.karvonen@utu.fi",
+                "subject": f"New contact from {request.data.get('name')}",
+                "text": f"Name: {request.data.get('name')}\nEmail: {request.data.get('email')}\nMessage: {request.data.get('message')}"
+            })
         except Exception as e:
             print(f"Email failed: {e}")
         return Response({'message': 'Message received!'}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
