924f06be4b83d0be7deaf7080c8fb417d167cd7a
Henok <49408901+henokcreative@users.noreply.github.com>
2026-03-08 01:16:32 +0200

    Remove unused dj_database_url import

--- labio/settings.py
+++ labio/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 import cloudinary
-import dj_database_url
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
